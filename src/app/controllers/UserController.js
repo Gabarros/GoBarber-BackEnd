3fd2bf895e620ac650221079a31a75a4cb4d9ca6
Gabarros <gabarros17@gmail.com>
2020-01-29 14:28:41 -0300

    Create user update route and authentication

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,6 +1,7 @@
 import User from '../models/User';
 
 class UserController {
+
   async store(req, res) {
 
     const userExists = await User.findOne({
@@ -22,6 +23,31 @@ class UserController {
       provider
     });
 
+  }
+
+  async update(req, res){
+    
+    const{ email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if(email !== user.email){
+
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+
+    }
+
+    if(oldPassword && !(await user.checkPassword(oldPassword))){
+      return res.status(401).json({ error: 'Password does not match'});
+    };
+
+    const { id, name, provider } = await user.update(req.body);
+
+    return res.json({ id, name, email, provider });
   }
 
 };
