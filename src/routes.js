6d48e542130bb54ad4afc97d6467d61a544b9096
Gabarros <gabarros17@gmail.com>
2020-01-29 12:13:52 -0300

    Create new user route

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-
-  const user = await User.create({
-    name: 'Gabriel',
-    email: 'gabarros17@gmail.com',
-    password_hash: '123456'
-  }).catch( err => {
-    res.status(400).json(err);
-  });
-
-  return res.json(user);
-  
-  
-});
+routes.post('/users', UserController.store);
 
 export default routes;
