460f3fa621da6497d51edfe13a9774fb650ed968
Gabarros <gabarros17@gmail.com>
2020-02-01 22:12:53 -0300

    Add MongoDb docker container to the project

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -12,6 +13,7 @@ class Database {
 
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -19,6 +21,12 @@ class Database {
 
     models.map(model => model.init(this.connection));
     models.map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo(){
+    this.mongoConnection = mongoose.connect(
+      'mongo://192.168.99.101:27017/gobarber',
+      { useNewUrlParser: true, useFindAndModify: true });
   }
 };
 
